6866919dbf03d8aec9aed124be93d11195cee7ab
lodestone-rock <lodestone.rock@gmail.com>
2024-07-18 14:00:30 +0700

    init

---
+++ compass.py
@@ -0,0 +1,84 @@
+import torch
+from torch.optim import Optimizer
+
+class Compass(Optimizer):
+    r"""
+    Arguments:
+        params (iterable): 
+            Iterable of parameters to optimize or dicts defining 
+            parameter groups.
+        lr (float): 
+            Learning rate parameter (default 0.0025)
+        betas (Tuple[float, float], optional): 
+            coefficients used for computing running averages of
+            gradient and its square (default: (0.9, 0.999)).
+        amp_fac (float):
+            amplification factor for the first moment filter (default: 2).
+        eps (float): 
+            Term added to the denominator outside of the root operation to 
+            improve numerical stability. (default: 1e-8).
+        weight_decay (float): 
+            Weight decay, i.e. a L2 penalty (default: 0).
+    """
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), amp_fac=2, eps=1e-8, weight_decay=0):
+        defaults = dict(lr=lr, betas=betas, amp_fac=amp_fac, eps=eps, weight_decay=weight_decay)
+        super(Compass, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('AdamW does not support sparse gradients, please consider SparseAdam instead')
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['ema'] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state['ema_squared'] = torch.zeros_like(p.data)
+
+                ema, ema_squared = state['ema'], state['ema_squared']
+                beta1, beta2 = group['betas']
+                amplification_factor = group['amp_fac']
+                lr = group['lr']
+                weight_decay = group['weight_decay']
+
+                state['step'] += 1
+
+                # Decay the first and second moment running average coefficient
+                # ema = ema + (1 - beta1) * grad
+                ema.mul_(beta1).add_(grad, alpha=1 - beta1)
+                # grad = grad + ema * amplification_factor
+                grad.add_(ema, alpha=amplification_factor)
+                # ema_squared = ema + (1 - beta2) * grad ** 2
+                ema_squared.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+                
+                # lr scaler + eps to prevent zero division
+                # denom = exp_avg_sq.sqrt() + group['eps']
+                denom = ema_squared.sqrt().add_(group['eps'])
+
+                # bias correction step size
+                # soft warmup
+                bias_correction = (1 - beta1 ** state['step'])
+                step_size = lr / bias_correction
+
+                # Weight decay on grad just like RMSProp
+                # TODO: not sure if this thing is correct! need to test first!
+                if weight_decay != 0:
+                    # grad = 
+                    grad.add_(p.data, alpha=weight_decay)
+                # p = p - lr * grad / denom  
+                p.data.addcdiv_(grad, denom, value=-step_size)               
+
+        return loss
