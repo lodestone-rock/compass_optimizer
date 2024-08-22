c607a383f51f66641652e9b3ad586b942ba574a4
lodestone-rock <lodestone.rock@gmail.com>
2024-08-22 10:24:02 +0700

    remove redundant function

--- experimental/compass_experimental_sr_bf16.py
+++ experimental/compass_experimental_sr_bf16.py
@@ -142,29 +142,6 @@ class Compasstic(Optimizer):
         return loss
 
 
-# @torch.compile
-def copy_stochastic_(target: torch.Tensor, source: torch.Tensor):
-    # thanks to Nerogar for fast stochastic pytorch implementation 
-    # https://github.com/pytorch/pytorch/issues/120376#issuecomment-1974828905
-    with torch.no_grad():
-        # create a random 16 bit integer
-        result = torch.randint_like(
-            source,
-            dtype=torch.int32,
-            low=0,
-            high=(1 << 16),
-        )
-
-        # add the random number to the lower 16 bit of the mantissa
-        result.add_(source.view(dtype=torch.int32))
-
-        # mask off the lower 16 bit of the mantissa
-        result.bitwise_and_(-65536)  # -65536 = FFFF0000 as a signed int32
-
-        # copy the higher 16 bit into the target tensor
-        target.copy_(result.view(dtype=torch.float32))
-
-
 class LPFAdamW(Optimizer):
     r"""
     Arguments:
